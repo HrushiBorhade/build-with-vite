c00d37cb0fff297be9efc82ae0c99a52613ab903
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-06 07:40:08 +0530

    feat: Add components to library

--- component-library/vite.config.ts
+++ component-library/vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path, { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { componentTagger } from "lovable-tagger";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      include: "/src",
+    }),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "component-library",
+      fileName: "component-library",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
