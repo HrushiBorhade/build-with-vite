b0b4b3cf1f41a37460ede063e28aaa492d4fd62a
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-10 20:39:55 +0530

    feat: Add vite inspect plugin for plugin debugging

--- element-tagger-plugin/vite.config.ts
+++ element-tagger-plugin/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "./element-tagger-pluign";
+import { componentTagger } from "./element-tagger-plugin";
 import path from "path";
+import Inspect from "vite-plugin-inspect";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), componentTagger()],
+  plugins: [react(), componentTagger(), Inspect()],
   resolve: {
     alias: {
       "@/": path.resolve(__dirname, "./src"),
